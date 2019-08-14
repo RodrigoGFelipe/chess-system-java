d85cf79479afc30953548a9ddb203d4b87c13213
Rodrigo Felipe <rodrigogfelipe@hotmail.com>
2019-08-14 12:55:39 -0300

    Possible moves of a piece

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -3,7 +3,7 @@ package chess;
 import boardgame.Board;
 import boardgame.Piece;
 
-public class ChessPiece extends Piece {
+public abstract class ChessPiece extends Piece {
 	private Color color; // Classe color do pacote enum
 
 	// Declarando o construtor
